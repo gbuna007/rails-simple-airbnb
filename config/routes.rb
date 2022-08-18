970f06efdc6516cfd05b2ed08ce2dfcb09821828
Grace Bunardi <gbunardi@hotmail.com>
2022-08-18 15:45:35 +0800

    crud complete, front end work pending

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'flats/index'
+  get 'flats/show'
+  get 'flats/edit'
+  get 'flats/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :flats
 end
